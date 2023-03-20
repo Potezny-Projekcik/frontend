63b8702312934f599fd7bcf47fa177d641a23d18
Piotr <pepapek@gmail.com>
2023-03-20 20:57:24 +0100

    Added libraries - i18n, router and mui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { I18nextProvider } from "react-i18next";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./configs/i18n";
+import i18next from "i18next";
 function App() {
-	return <div className="App"></div>;
+	return (
+		<I18nextProvider i18n={i18next}>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<div></div>} />
+				</Routes>
+			</BrowserRouter>
+		</I18nextProvider>
+	);
 }
 
 export default App;
