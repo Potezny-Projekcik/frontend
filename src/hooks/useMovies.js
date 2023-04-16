b2c42dfd1edafb5fd521b2652c6c56d59c48bfae
Piotr <pepapek@gmail.com>
2023-04-16 14:17:29 +0200

    More than one page fetched

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,5 +1,6 @@
 import { useCallback, useEffect, useState } from "react";
 import axios from "axios";
+const MOVIE_PAGES_AMOUNT = 3;
 
 function getMoviesPromises(movies) {
 	return movies.map(async ({ title, release_date, id }) => {
@@ -31,10 +32,19 @@ export default function useMovies() {
 	const [movies, setMovies] = useState([]);
 	const fetchMovies = useCallback(async () => {
 		try {
-			const response = await axios.get(
-				"https://api.themoviedb.org/3/movie/popular?api_key=8e36936e04ae03c485e9b156d52c6c39"
+			const pagesArray = Array.from(
+				{ length: MOVIE_PAGES_AMOUNT },
+				(_, i) => i + 1
 			);
-			const moviesPromises = getMoviesPromises(response.data.results);
+			const requests = pagesArray.map((page) =>
+				axios.get(
+					`https://api.themoviedb.org/3/movie/popular?api_key=8e36936e04ae03c485e9b156d52c6c39&page=${page}`
+				)
+			);
+
+			const results = await Promise.all(requests);
+			const moviesResults = results.flatMap((result) => result.data.results);
+			const moviesPromises = getMoviesPromises(moviesResults);
 			const moviesFromApi = await Promise.all(moviesPromises);
 			setMovies(moviesFromApi);
 		} catch (err) {
