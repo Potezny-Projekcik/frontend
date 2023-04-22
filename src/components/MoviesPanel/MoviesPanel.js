cd227e26836d8810ebff97d318a506c7b2ed4015
Piotr <pepapek@gmail.com>
2023-04-22 10:34:01 +0200

    console.log after scroll to bottom

--- src/components/MoviesPanel/MoviesPanel.js
+++ src/components/MoviesPanel/MoviesPanel.js
@@ -1,9 +1,36 @@
 import { Grid } from "@mui/material";
 import useMovies from "../../hooks/useMovies";
 import Movie from "../Movie/Movie";
+import { useState, useEffect } from "react";
 
 function MoviesPanel() {
 	const { movies } = useMovies();
+	const [isBottom, setIsBottom] = useState(false);
+
+	const handleScroll = () => {
+		const { scrollY, innerHeight } = window;
+		const margin = 200;
+		const isScrolledToBottom =
+			scrollY + innerHeight + margin >= document.body.offsetHeight;
+		if (isScrolledToBottom) {
+			setIsBottom(true);
+			yourFunction();
+		} else {
+			setIsBottom(false);
+		}
+	};
+
+	useEffect(() => {
+		window.addEventListener("scroll", handleScroll);
+
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, []);
+
+	const yourFunction = () => {
+		console.log("Użytkownik zjechał scrollem na sam dół strony");
+	};
 	return (
 		<Grid container spacing={{ xs: 4, md: 5 }}>
 			{movies.map((movie) => (
