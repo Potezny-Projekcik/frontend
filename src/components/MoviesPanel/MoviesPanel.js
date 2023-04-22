0a94b5db531bad8b2e198030a5a461599afe549c
Piotr <pepapek@gmail.com>
2023-04-22 11:10:24 +0200

    It works but code is ugly

--- src/components/MoviesPanel/MoviesPanel.js
+++ src/components/MoviesPanel/MoviesPanel.js
@@ -5,32 +5,7 @@ import { useState, useEffect } from "react";
 
 function MoviesPanel() {
 	const { movies } = useMovies();
-	const [isBottom, setIsBottom] = useState(false);
 
-	const handleScroll = () => {
-		const { scrollY, innerHeight } = window;
-		const margin = 200;
-		const isScrolledToBottom =
-			scrollY + innerHeight + margin >= document.body.offsetHeight;
-		if (isScrolledToBottom) {
-			setIsBottom(true);
-			yourFunction();
-		} else {
-			setIsBottom(false);
-		}
-	};
-
-	useEffect(() => {
-		window.addEventListener("scroll", handleScroll);
-
-		return () => {
-			window.removeEventListener("scroll", handleScroll);
-		};
-	}, []);
-
-	const yourFunction = () => {
-		console.log("Użytkownik zjechał scrollem na sam dół strony");
-	};
 	return (
 		<Grid container spacing={{ xs: 4, md: 5 }}>
 			{movies.map((movie) => (
--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -28,29 +28,59 @@ function getMoviesPromises(movies) {
 		}
 	});
 }
+const getMoviesFromApi = async (pagesArray) => {
+	const requests = pagesArray.map((page) =>
+		axios.get(
+			`https://api.themoviedb.org/3/movie/popular?api_key=8e36936e04ae03c485e9b156d52c6c39&page=${page}`
+		)
+	);
+
+	const results = await Promise.all(requests);
+	const moviesResults = results.flatMap((result) => result.data.results);
+	const moviesPromises = getMoviesPromises(moviesResults);
+	const moviesFromApi = await Promise.all(moviesPromises);
+	return moviesFromApi;
+};
 export default function useMovies() {
 	const [movies, setMovies] = useState([]);
+	const [isBottom, setIsBottom] = useState(false);
+	// const [moviesAmount, setMoviesAmount] = useState(MOVIE_PAGES_AMOUNT);
+	let moviesAmount = MOVIE_PAGES_AMOUNT;
+
 	const fetchMovies = useCallback(async () => {
 		try {
 			const pagesArray = Array.from(
-				{ length: MOVIE_PAGES_AMOUNT },
-				(_, i) => i + 1
+				{ length: moviesAmount - movies.length },
+				(_, i) => i + movies.length + 1
 			);
-			const requests = pagesArray.map((page) =>
-				axios.get(
-					`https://api.themoviedb.org/3/movie/popular?api_key=8e36936e04ae03c485e9b156d52c6c39&page=${page}`
-				)
-			);
-
-			const results = await Promise.all(requests);
-			const moviesResults = results.flatMap((result) => result.data.results);
-			const moviesPromises = getMoviesPromises(moviesResults);
-			const moviesFromApi = await Promise.all(moviesPromises);
+			console.log(pagesArray);
+			const moviesFromApi = await getMoviesFromApi(pagesArray);
 			setMovies(moviesFromApi);
 		} catch (err) {
 			console.error(err);
 		}
 	}, []);
+	const handleScroll = useCallback(() => {
+		const { scrollY, innerHeight } = window;
+		const margin = 200;
+		const isScrolledToBottom =
+			scrollY + innerHeight + margin >= document.body.offsetHeight;
+		if (isScrolledToBottom && !isBottom) {
+			setIsBottom(true);
+			// setMoviesAmount(moviesAmount + 1);
+			moviesAmount++;
+			fetchMovies();
+		} else {
+			setIsBottom(false);
+		}
+	}, []);
+	useEffect(() => {
+		window.addEventListener("scroll", handleScroll);
+
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, [handleScroll]);
 
 	useEffect(() => {
 		fetchMovies();
