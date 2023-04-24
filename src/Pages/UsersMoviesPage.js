c4db06c15c7e3b16a0119dc481ce06773f675e72
Radosz <radosz.werner@gmail.com>
2023-04-24 13:22:48 +0200

    Some first pages

---
+++ src/Pages/UsersMoviesPage.js
@@ -0,0 +1,12 @@
+import MoviesPanel from "../components/UserMoviesPanel/UserMoviesPanel";
+import UIContainer from "./UIContainer";
+
+function MoviesPage() {
+  return (
+    <UIContainer>
+      <UserMoviesPanel></UserMoviesPanel>
+    </UIContainer>
+  );
+}
+
+export default MoviesPage;
