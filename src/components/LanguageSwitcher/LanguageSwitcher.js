a2fedccfb78eac594f8dfb4bb23648a3e55cf3ee
Wojciech Tobolski <72449959+Wojciech-Tobolski@users.noreply.github.com>
2023-05-19 17:34:16 +0100

    after review

--- src/components/LanguageSwitcher/LanguageSwitcher.js
+++ src/components/LanguageSwitcher/LanguageSwitcher.js
@@ -9,38 +9,24 @@ import LanguageIcon from '@mui/icons-material/Language';
 import ExpandLess from '@mui/icons-material/ExpandLess';
 import ExpandMore from '@mui/icons-material/ExpandMore';
 import Collapse from '@mui/material/Collapse';
+import LanguageListItem from './LanguageListItem';
 
 export default function LanguageSwitcher() {
   const [open, setOpen] = useState(false);
-  const {t,i18n } = useTranslation("LanguageSwitcher");
-
-  const handleLanguageChange = (lng) => {
-    i18n.changeLanguage(lng);
-  };
-  
+  const {t} = useTranslation("LanguageSwitcher");
   return (
-    <List
+    <List 
       sx={{ width: '100%', maxWidth: 200, borderRadius: "10px",bgcolor: '#81d4fa'}}
-      component="nav"
-    >
+      component="nav">
       <ListItemButton onClick={() => setOpen(!open)}>
-        <ListItemIcon>
-        <LanguageIcon/>
-        </ListItemIcon> 
+        <ListItemIcon><LanguageIcon/></ListItemIcon> 
         <ListItemText primary={`${t("language")}`} sx={{textAlign: 'center'}}></ListItemText>
         {open ? <ExpandLess /> : <ExpandMore />}
       </ListItemButton>
       <Collapse in={open} timeout="auto" unmountOnExit>
         <List component="div" disablePadding>
-          <ListItemButton sx={{ pl: 4, bgcolor: '#b3e5fc'}} onClick={() => handleLanguageChange('en')}>
-            <ListItemText primary={`${t("english")}`} sx={{ textAlign: 'center'}}
-            primaryTypographyProps={{ fontWeight: i18n.language === 'en' ? 'bold' : 'inherit'}}
-            />
-          </ListItemButton>
-          <ListItemButton sx={{ pl: 4, bgcolor: '#b3e5fc' }} onClick={() => handleLanguageChange('pl')}>
-            <ListItemText primary={`${t('polish')}`} sx={{ textAlign: 'center'}}
-            primaryTypographyProps={{ fontWeight: i18n.language === 'pl' ? 'bold' : 'inherit'}}/>
-          </ListItemButton>
+        <LanguageListItem langCode="en"/>
+        <LanguageListItem langCode="pl"/>
         </List>
       </Collapse>
     </List>
