c53faf9b98ed7587d530116ff5c845eff85dbee8
Wojciech Tobolski <72449959+Wojciech-Tobolski@users.noreply.github.com>
2023-05-19 18:47:35 +0100

    after rework

--- src/Pages/MoviesPage.js
+++ src/Pages/MoviesPage.js
@@ -1,11 +1,11 @@
 import MoviesPanel from "../components/Panels/MoviesPanel";
 import UIContainer from "./UIContainer";
-import LanguageSwitcher from "../components/LanguageSwitcher/LanguageSwitcher";
+
 
 const MoviesPage = () => {
 	return (
 		<UIContainer>
-			<LanguageSwitcher />
+
 			<MoviesPanel></MoviesPanel>
 		</UIContainer>
 	);
